7334dc4f1b95a47b7b9527fb49cbdf1eb67e2d88
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2025-01-29 14:53:10 +0100

    ✨ Add network status indicator to Header component; implement useNetwork hook for online/offline detection

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,10 +1,13 @@
-import { CircleArrowDown, Home, Search } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
 import React from 'react';
+import Image from 'next/image';
+import useNetwork from '@/hooks/useNetwork';
 import SearchBar from '../UI/SearchBar';
+import { Wifi, WifiOff } from 'lucide-react';
 
 const Header = () => {
+  const isOnline = useNetwork();
+
   return (
     <header className="h-16 w-screen text-zinc-400 flex items-center justify-between px-4">
       <Image
@@ -14,6 +17,15 @@ const Header = () => {
         height={24}
       />
       <SearchBar />
+      {isOnline ? (
+        <div className="bg-green-500 text-white rounded-full p-1">
+          <Wifi />
+        </div>
+      ) : (
+        <div className="bg-red-500 text-white rounded-full p-1">
+          <WifiOff />
+        </div>
+      )}
     </header>
   );
 };
