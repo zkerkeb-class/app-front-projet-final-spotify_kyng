222ffe88a76f2c2807af5ea234aa6676e60509c2
Inès Gervais <71017446+Plouski@users.noreply.github.com>
2025-02-04 15:39:58 +0100

    Manifest + PWA + HTTPS OK

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,47 @@
-// service-worker.js
 self.addEventListener('install', (event) => {
-    console.log('Service Worker Installed');
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker Activated');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
-    );
-  });
-  
+  console.log('Service Worker Installed');
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles/main.css',
+        '/scripts/main.js',
+        '/web-app-manifest-192x192.png',
+        '/web-app-manifest-512x512.png',
+        '/favicon.ico',
+        '/images/logo.png',
+      ]);
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker Activated');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'v1') {
+            console.log('Service Worker: Clearing old cache');
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((response) => {
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+const https = require('https');
+const fs = require('fs');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync('./certs/localhost-key.pem'),
+        cert: fs.readFileSync('./certs/localhost-cert.pem'),
+      },
+      (req, res) => {
+        handle(req, res);
+      }
+    )
+    .listen(3000, (err) => {
+      if (err) throw err;
+      console.log('> Ready on https://localhost:3000');
+    });
+});
